eb86a208cc0edcb2acb1997b72aa59b98e19b6d6
michaelvalenzuela <20mvalen@gmail.com>
2020-12-11 15:03:29 -0800

    Added move feature and move properties

--- main.js
+++ main.js
@@ -4,22 +4,32 @@ let position = $carImg.getBoundingClientRect();
 let car = {
   locationX: position.x,
   locationY: position.y,
-  isStarted: false
+  isStarted: false,
+  moveX: 1,
+  moveY: 0
 }
 
 
 document.addEventListener("keydown", function(e){
   if(e.key === "ArrowDown"){
     $carImg.style.transform = "rotate(0deg) rotate(90deg)";
+    car.moveX = 0;
+    car.moveY = 1;
   }
   else if(e.key === "ArrowLeft"){
     $carImg.style.transform = "rotate(0deg) rotate(180deg)";
+    car.moveX = -1;
+    car.moveY = 0;
   }
   else if(e.key === "ArrowRight"){
     $carImg.style.transform = "rotate(0deg)";
+    car.moveX = 1;
+    car.moveY = 0;
   }
   else if(e.key === "ArrowUp"){
     $carImg.style.transform = "rotate(0deg) rotate(-90deg)";
+    car.moveX = 0;
+    car.moveY = -1;
   }
   else if (e.key === " "){
     if(!car.isStarted){
@@ -33,15 +43,17 @@ document.addEventListener("keydown", function(e){
 
 function startCar(){
   car.isStarted = true;
-  moveCarId = setInterval(function(){
-    car.locationX += 1;
-    car.locationY += 0;
-    $carImg.style.top = car.locationY + "px";
-    $carImg.style.left = car.locationX + "px";
-  }, 16);
+  moveCarId = setInterval(moveCar, 16);
 }
 
 function stopCar(){
   car.isStarted = false;
   clearInterval(moveCarId);
+}
+
+function moveCar(){
+  car.locationX += car.moveX;
+  car.locationY += car.moveY;
+  $carImg.style.top = car.locationY + "px";
+  $carImg.style.left = car.locationX + "px";
 }
