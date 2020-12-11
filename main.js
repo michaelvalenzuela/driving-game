adb36ede137e804fea7ec2716f067912e6efa828
michaelvalenzuela <20mvalen@gmail.com>
2020-12-11 14:35:01 -0800

    Created listener and startCar function

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 let $carImg = document.getElementById("f1Car");
-
+let moveCarId = null;
+let position = $carImg.getBoundingClientRect();
 let car = {
-  locationX: 0,
-  locationY: 0
+  locationX: position.x,
+  locationY: position.y
 }
 
+
 document.addEventListener("keydown", function(e){
+  console.log(e);
   if(e.key === "ArrowDown"){
     $carImg.style.transform = "rotate(0deg) rotate(90deg)";
   }
@@ -18,4 +21,16 @@ document.addEventListener("keydown", function(e){
   else if(e.key === "ArrowUp"){
     $carImg.style.transform = "rotate(0deg) rotate(-90deg)";
   }
+  else if (e.key === " "){
+    startCar();
+  }
 });
+
+function startCar(){
+  moveCarId = setInterval(function(e){
+    car.locationX += 1;
+    car.locationY += 0;
+    $carImg.style.top = car.locationY + "px";
+    $carImg.style.left = car.locationX + "px";
+  }, 16);
+}
