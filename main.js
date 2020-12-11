e7113ccc4684a59089be2de31ad5cf6f6b018ac8
michaelvalenzuela <20mvalen@gmail.com>
2020-12-11 13:10:50 -0800

    Added event listener to rotate car

---
+++ main.js
@@ -0,0 +1,16 @@
+let $carImg = document.getElementById("f1Car");
+
+document.addEventListener("keydown", function(e){
+  if(e.key === "ArrowDown"){
+    $carImg.style.transform = "rotate(0deg) rotate(90deg)";
+  }
+  else if(e.key === "ArrowLeft"){
+    $carImg.style.transform = "rotate(0deg) rotate(180deg)";
+  }
+  else if(e.key === "ArrowRight"){
+    $carImg.style.transform = "rotate(0deg)";
+  }
+  else if(e.key === "ArrowUp"){
+    $carImg.style.transform = "rotate(0deg) rotate(-90deg)";
+  }
+});
