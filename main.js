7ed629e0f5d1e30618dd89740cd086146219975a
michaelvalenzuela <20mvalen@gmail.com>
2020-12-11 13:20:27 -0800

    Added car obj and location properties

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 let $carImg = document.getElementById("f1Car");
 
+let car = {
+  locationX: 0,
+  locationY: 0
+}
+
 document.addEventListener("keydown", function(e){
   if(e.key === "ArrowDown"){
     $carImg.style.transform = "rotate(0deg) rotate(90deg)";
